b7d476edda7cd2a864093a01c959dfdaf286da36
anishs <anish2sinha@gmail.com>
2024-11-10 21:02:05 -0500

    implement keycloak image to deploy on openshift, enabling login

--- app/src/keycloak.jsx
+++ app/src/keycloak.jsx
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://127.0.0.1:8080', 
+    url: 'https://keycloak-autopilot-dashboard-f3dc9e.apps.shift.nerc.mghpcc.org',
     realm: 'autopilot-dashboard',
     clientId: 'autopilot-dashboard',
 });
