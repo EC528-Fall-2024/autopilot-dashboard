8b3e251b4845fecf7c3ab15542a5dbe47f10fd95
Joshua Arrevillaga <joshuaarrevillaga@crc-dot1x-nat-10-239-196-24.bu.edu>
2024-11-14 14:13:32 -0500

    Made some slight modifications to the .json, added the local links for the project to test github capabilities

--- app/src/keycloak.jsx
+++ app/src/keycloak.jsx
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'https://keycloak-autopilot-dashboard-f3dc9e.apps.shift.nerc.mghpcc.org',
+    url: 'http://localhost:8080',
     realm: 'autopilot-dashboard',
     clientId: 'autopilot-dashboard',
 });
